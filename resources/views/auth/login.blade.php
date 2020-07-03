99580ca9f27688d7771705cdaef13c76e74b75a5
Diego Vargas <diego@soho.cl>
2020-07-02 23:36:49 -0500

    fix login email label

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,7 +13,7 @@
 
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">
-                              <i class="fa fa-envelope-o" aria-hidden="true"></i> E-Mail Prueba
+                              <i class="fa fa-envelope-o" aria-hidden="true"></i> E-Mail
                             </label>
 
                             <div class="col-md-6">
